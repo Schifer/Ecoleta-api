dec7e5233c39ad863e28ccb6251ce5501072f9ac
Octavio Arruda <octavio@maxiprod.com.br>
2020-06-03 23:01:31 -0300

    added knex, sqlite connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, { request } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 // Rota: Endereço completo da requisição
 // Recurso: Qual entidade estamos acessando do sistema
 
@@ -16,41 +19,5 @@ app.use(express.json());
 // Query Param: Parâmetros que vem na prória rota, geralmente opcionais, 
 // para filtros, paginação, etc
 // Request Body: Parâmetros para criação e atualização de informações
-
-const users = [
-    'Octavio', // 0
-    'Eduardo', // 1
-    'Hyago',   // 2
-    'José',    // 3
-    'Daniel',  // 4
-];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    // Javascript object notation - JSON
-
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    
-    const usuario = users[id];
-
-    return response.json(usuario);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email,
-    };
-
-    return response.json(user);
-});
 
 app.listen(3333);
